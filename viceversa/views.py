3fd93575f34eb1903c1b4b1a047b89419ed0ce18
fusetentacl <lfasghjkl@gmail.com>
2021-07-14 15:42:08 +0500

    reverse function

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,5 +1,10 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	count_words = len(user_text.split())
+	reverse_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text, 'countwords': count_words})
